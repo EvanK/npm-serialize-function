a2f13185b557362c2d7ade96807d9415fe3e2106
Evan Kaufman <84145+EvanK@users.noreply.github.com>
2026-06-19 14:33:52 -0700

    Non-regex comment stripping

--- src/main.mjs
+++ src/main.mjs
@@ -72,6 +72,92 @@ function getConstructor(type) {
   }
 }
 
+// non-regex comment stripping
+// source: https://j11y.io/javascript/removing-comments-in-javascript/
+function commentStripper(str) {
+    str = ('__' + str + '__').split('');
+    const mode = {
+        singleQuote: false,
+        doubleQuote: false,
+        regex: false,
+        blockComment: false,
+        lineComment: false,
+        condComp: false 
+    };
+    for (var i = 0, l = str.length; i < l; i++) {
+ 
+        if (mode.regex) {
+            if (str[i] === '/' && str[i-1] !== '\\') {
+                mode.regex = false;
+            }
+            continue;
+        }
+ 
+        if (mode.singleQuote) {
+            if (str[i] === "'" && str[i-1] !== '\\') {
+                mode.singleQuote = false;
+            }
+            continue;
+        }
+ 
+        if (mode.doubleQuote) {
+            if (str[i] === '"' && str[i-1] !== '\\') {
+                mode.doubleQuote = false;
+            }
+            continue;
+        }
+ 
+        if (mode.blockComment) {
+            if (str[i] === '*' && str[i+1] === '/') {
+                str[i+1] = '';
+                mode.blockComment = false;
+            }
+            str[i] = '';
+            continue;
+        }
+ 
+        if (mode.lineComment) {
+            if (str[i+1] === 'n' || str[i+1] === 'r') {
+                mode.lineComment = false;
+            }
+            str[i] = '';
+            continue;
+        }
+ 
+        if (mode.condComp) {
+            if (str[i-2] === '@' && str[i-1] === '*' && str[i] === '/') {
+                mode.condComp = false;
+            }
+            continue;
+        }
+ 
+        mode.doubleQuote = str[i] === '"';
+        mode.singleQuote = str[i] === "'";
+ 
+        if (str[i] === '/') {
+ 
+            if (str[i+1] === '*' && str[i+2] === '@') {
+                mode.condComp = true;
+                continue;
+            }
+            if (str[i+1] === '*') {
+                str[i] = '';
+                mode.blockComment = true;
+                continue;
+            }
+            if (str[i+1] === '/') {
+                str[i] = '';
+                mode.lineComment = true;
+                continue;
+            }
+            mode.regex = true;
+ 
+        }
+ 
+    }
+    return str.join('').slice(2, -2);
+}
+
 function serialize(func, opts) {
   const def = { hash: false, comments: false, whitespace: false };
   opts = (typeof opts === 'object' && null !== opts)
@@ -91,11 +177,7 @@ function serialize(func, opts) {
 
   // strip any comments
   if (!opts.comments) {
-    stringified = stringified
-      .replace(/("([^\\"]|\\")*")|('([^\\']|\\')*')/g, (m) => m.replace(/\//g, '\1'))
-      .replace(/\/\*[\s\S]*?\*\/|(?<=[^:])\/\/.*|^\/\/.*/g,'')
-      .replace(/\1/g, '/')
-    ;
+    stringified = commentStripper(stringified);
   }
 
   // strip leading/trailing whitespace from each line
